4b2d4d0aa5c53e63e8f3b07b7c1c9d60b8e912ef
yr.deng <yr.deng@cloudchip.com>
2018-08-10 18:37:50 +0800

    [ppcrong.20180810][UNITYEXPORT] 1. Use Text to replace TextMesh 2. Add setXXXType and setMoveScale APIs 3. Fine tune scene UI 4. Set orientation to landscape

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    // Text on UI
+    public Text mTextCx;
+    public Text mTextCy;
+    public Text mTextCz;
+
+    // Helicopter
+    public GameObject mPlayer;
+
+    // Offset between player and camera
+    private Vector3 mOffset;
+
+    // For SmoothDamp
+    public float mSmoothTime = 0.05F;
+    private Vector3 mVelocity = Vector3.zero;
+
+    // For Lerp
+    public float mMoveInterpolant = 0.1F;
+
+    // If camera should follow player
+    public bool mIsFollowPlayer = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        mOffset = transform.position - mPlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Current player position
+        mTextCx.text = string.Format("Cx: {0:0.0000}", transform.position.x);
+        mTextCy.text = string.Format("Cy: {0:0.0000}", transform.position.y);
+        mTextCz.text = string.Format("Cz: {0:0.0000}", transform.position.z);
+
+        if (mIsFollowPlayer)
+        {
+            SmoothMove();
+        }
+    }
+
+    // SmoothDamp: https://docs.unity3d.com/ScriptReference/Vector3.SmoothDamp.html
+    // Lerp: https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+    void SmoothMove()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = mPlayer.transform.position + mOffset;
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref mVelocity, mSmoothTime);
+
+        // Use Lerp
+        //transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, moveInterpolant);
+    }
+
+    // Export to set if camera follow player
+    public void setIsFollowPlayer(string b)
+    {
+        mIsFollowPlayer = bool.Parse(b);
+    }
+
+    // Export to set interpolant movement
+    public void setMoveSmoothTime(string time)
+    {
+        mSmoothTime = float.Parse(time);
+    }
+
+    // Export to set interpolant movement
+    public void setMoveInterpolant(string interpolant)
+    {
+        mMoveInterpolant = float.Parse(interpolant);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     // Text on UI
-    public TextMesh mTextQuaternionX;
-    public TextMesh mTextQuaternionY;
-    public TextMesh mTextQuaternionZ;
-    public TextMesh mTextQuaternionW;
-    public TextMesh mTextMovementX;
-    public TextMesh mTextMovementY;
-    public TextMesh mTextMovementZ;
+    public Text mTextQx;
+    public Text mTextQy;
+    public Text mTextQz;
+    public Text mTextQw;
+    public Text mTextMx;
+    public Text mTextMy;
+    public Text mTextMz;
+    public Text mTextPx;
+    public Text mTextPy;
+    public Text mTextPz;
 
     // Interpolant
-    public float mInterpolantQuaternion;
-    public float mInterpolantMovement;
+    public float mRotateInterpolant = 0.8f;
+    public float mMoveInterpolant = 0.1F;
 
     // Quaternion and Movement values
     public float mX, mY, mZ, mQx, mQy, mQz, mQw;
 
+    /*
+     * Rotate type
+     * 0: Direct
+     * 1: Smooth
+     */
+    public int mRotateType = 0;
+
+    /*
+     * Move type
+     * 0: Direct
+     * 1: Smooth
+     */
+    public int mMoveType = 1;
+
+    public float mMoveScale = 1F;
+
     // Use this for initialization
     void Start()
     {
-        mTextMovementX = GameObject.Find("MovementX").GetComponent<TextMesh>();
-        mTextMovementY = GameObject.Find("MovementY").GetComponent<TextMesh>();
-        mTextMovementZ = GameObject.Find("MovementZ").GetComponent<TextMesh>();
-        mTextQuaternionX = GameObject.Find("QuaternionX").GetComponent<TextMesh>();
-        mTextQuaternionY = GameObject.Find("QuaternionY").GetComponent<TextMesh>();
-        mTextQuaternionZ = GameObject.Find("QuaternionZ").GetComponent<TextMesh>();
-        mTextQuaternionW = GameObject.Find("QuaternionW").GetComponent<TextMesh>();
     }
 
-    bool b = true;
     // Update is called once per frame
     void Update()
     {
         // Update helicopter rotation and position
-        SmoothMove(mX, mY, mZ);
-        SmoothRotate(mQx, mQy, mQz, mQw);
+        switch (mRotateType)
+        {
+            case 0:
+            default:
+                Rotate(mQx, mQy, mQz, mQw);
+                break;
+            case 1:
+                SmoothRotate(mQx, mQy, mQz, mQw);
+                break;
+        }
+        switch (mMoveType)
+        {
+            case 0:
+            default:
+                Move(mX, mY, mZ);
+                break;
+            case 1:
+                SmoothMove(mX, mY, mZ);
+                break;
+        }
 
         // Leave app when pressing back/home keys
         if (Application.platform == RuntimePlatform.Android)
@@ -61,21 +91,22 @@ public class PlayerController : MonoBehaviour
     #region Rotation
     private void Rotate(float x, float y, float z, float w)
     {
-        GetComponent<Transform>().rotation = new Quaternion(x, y, z, w);
-        mTextQuaternionX.text = string.Format("Qx: {0:0.0000}", x);
-        mTextQuaternionY.text = string.Format("Qy: {0:0.0000}", y);
-        mTextQuaternionZ.text = string.Format("Qz: {0:0.0000}", z);
-        mTextQuaternionW.text = string.Format("Qw: {0:0.0000}", w);
+        transform.rotation = new Quaternion(x, y, z, w);
+
+        mTextQx.text = string.Format("Qx: {0:0.0000}", x);
+        mTextQy.text = string.Format("Qy: {0:0.0000}", y);
+        mTextQz.text = string.Format("Qz: {0:0.0000}", z);
+        mTextQw.text = string.Format("Qw: {0:0.0000}", w);
     }
 
     private void SmoothRotate(float x, float y, float z, float w)
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(x, y, z, w), mInterpolantQuaternion);
+        transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(x, y, z, w), mRotateInterpolant);
 
-        mTextQuaternionX.text = string.Format("Qx: {0:0.0000}", x);
-        mTextQuaternionY.text = string.Format("Qy: {0:0.0000}", y);
-        mTextQuaternionZ.text = string.Format("Qz: {0:0.0000}", z);
-        mTextQuaternionW.text = string.Format("Qw: {0:0.0000}", w);
+        mTextQx.text = string.Format("Qx: {0:0.0000}", x);
+        mTextQy.text = string.Format("Qy: {0:0.0000}", y);
+        mTextQz.text = string.Format("Qz: {0:0.0000}", z);
+        mTextQw.text = string.Format("Qw: {0:0.0000}", w);
     }
 
     // Export to set rotation
@@ -92,37 +123,52 @@ public class PlayerController : MonoBehaviour
             mQy = float.Parse(values[1]);
             mQz = float.Parse(values[2]);
             mQw = float.Parse(values[3]);
-            //Rotate(x, y, z, w);
-            //mTextQuaternionX.text = string.Format("Qx: {0:0.0000}", x);
-            //mTextQuaternionY.text = string.Format("Qy: {0:0.0000}", y);
-            //mTextQuaternionZ.text = string.Format("Qz: {0:0.0000}", z);
-            //mTextQuaternionW.text = string.Format("Qw: {0:0.0000}", w);
         }
     }
 
     // Export to set interpolant quaternion
-    public void setQuaternionT(string t)
+    public void setRotateInterpolant(string interpolant)
     {
-        mInterpolantQuaternion = float.Parse(t);
+        mRotateInterpolant = float.Parse(interpolant);
+    }
+
+    // Export to set rotate type
+    public void setRotateType(string type)
+    {
+        mRotateType = int.Parse(type);
     }
     #endregion
 
     #region Movement
     private void Move(float x, float y, float z)
     {
-        GetComponent<Transform>().position = new Vector3(x, y, z);
-        mTextMovementX.text = string.Format("X: {0:0.0000}", x);
-        mTextMovementY.text = string.Format("Y: {0:0.0000}", y);
-        mTextMovementZ.text = string.Format("Z: {0:0.0000}", z);
+        transform.position = new Vector3(x * mMoveScale, y * mMoveScale, z * mMoveScale);
+
+        // Movement
+        mTextMx.text = string.Format("X: {0:0.0000}", x);
+        mTextMy.text = string.Format("Y: {0:0.0000}", y);
+        mTextMz.text = string.Format("Z: {0:0.0000}", z);
+
+        // Current player position
+        mTextPx.text = string.Format("Px: {0:0.0000}", transform.position.x);
+        mTextPy.text = string.Format("Py: {0:0.0000}", transform.position.y);
+        mTextPz.text = string.Format("Pz: {0:0.0000}", transform.position.z);
     }
 
     private void SmoothMove(float x, float y, float z)
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, z), mInterpolantMovement);
+        transform.position = Vector3.Lerp(transform.position,
+            new Vector3(x * mMoveScale, y * mMoveScale, z * mMoveScale), mMoveInterpolant);
 
-        mTextMovementX.text = string.Format("X: {0:0.0000}", x);
-        mTextMovementY.text = string.Format("Y: {0:0.0000}", y);
-        mTextMovementZ.text = string.Format("Z: {0:0.0000}", z);
+        // Movement
+        mTextMx.text = string.Format("X: {0:0.0000}", x);
+        mTextMy.text = string.Format("Y: {0:0.0000}", y);
+        mTextMz.text = string.Format("Z: {0:0.0000}", z);
+
+        // Current player position
+        mTextPx.text = string.Format("Px: {0:0.0000}", transform.position.x);
+        mTextPy.text = string.Format("Py: {0:0.0000}", transform.position.y);
+        mTextPz.text = string.Format("Pz: {0:0.0000}", transform.position.z);
     }
 
     // Export to set movement
@@ -138,17 +184,25 @@ public class PlayerController : MonoBehaviour
             mX = float.Parse(values[0]);
             mY = float.Parse(values[1]);
             mZ = float.Parse(values[2]);
-            //Move(x, y, z);
-            //mTextMovementX.text = string.Format("X: {0:0.0000}", x);
-            //mTextMovementY.text = string.Format("Y: {0:0.0000}", y);
-            //mTextMovementZ.text = string.Format("Z: {0:0.0000}", z);
         }
     }
 
     // Export to set interpolant movement
-    public void setMovementT(string t)
+    public void setMoveInterpolant(string interpolant)
     {
-        mInterpolantMovement = float.Parse(t);
+        mMoveInterpolant = float.Parse(interpolant);
+    }
+
+    // Export to set move type
+    public void setMoveType(string type)
+    {
+        mMoveType = int.Parse(type);
+    }
+
+    // Export to set move scale
+    public void setMoveScale(string scale)
+    {
+        mMoveScale = float.Parse(scale);
     }
     #endregion
 }
