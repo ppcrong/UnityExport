78cfe03e1ca7ae09c30eb103d97de704335a5c0f
yr.deng <yr.deng@cloudchip.com>
2018-09-03 18:31:33 +0800

    [ppcrong.20180903][UNITYEXPORT] Refactor code and add Eular rotation feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    // Text on UI
+    // Rotate Text
     public Text mTextQx;
     public Text mTextQy;
     public Text mTextQz;
     public Text mTextQw;
+    public Text mTextEx;
+    public Text mTextEy;
+    public Text mTextEz;
+
+    // Move Text
     public Text mTextMx;
     public Text mTextMy;
     public Text mTextMz;
@@ -23,12 +28,14 @@ public class PlayerController : MonoBehaviour
     public float mMoveInterpolant = 0.1F;
 
     // Quaternion and Movement values
-    public float mX, mY, mZ, mQx, mQy, mQz, mQw;
+    public float mX, mY, mZ, mQx, mQy, mQz, mQw, mEx, mEy, mEz;
 
     /*
      * Rotate type
-     * 0: Direct
-     * 1: Smooth
+     * 0: Direct Quaternion
+     * 1: Smooth Quaternion
+     * 2: Direct Eular
+     * 3: Smooth Eular
      */
     public int mRotateType = 0;
 
@@ -63,71 +70,72 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // Update helicopter rotation and position
-        switch (mRotateType)
-        {
-            case 0:
-            default:
-                Rotate(mQx, mQy, mQz, mQw);
-                break;
-            case 1:
-                SmoothRotate(mQx, mQy, mQz, mQw);
-                break;
-        }
-        switch (mMoveType)
-        {
-            case 0:
-            default:
-                Move(mX, mY, mZ);
-                break;
-            case 1:
-                SmoothMove(mX, mY, mZ);
-                break;
-        }
+        Rotate(mRotateType, mQx, mQy, mQz, mQw, mEx, mEy, mEz);
+        Move(mMoveType, mX, mY, mZ);
     }
 
     #region Rotation
-    private void Rotate(float x, float y, float z, float w)
+    private void Rotate(int type, float qx, float qy, float qz, float qw,
+        float ex, float ey, float ez)
     {
-        transform.rotation = new Quaternion(x, y, z, w);
-
-        try
+        switch (type)
         {
-            mTextQx.text = string.Format("Qx: {0:0.0000}", x);
-            mTextQy.text = string.Format("Qy: {0:0.0000}", y);
-            mTextQz.text = string.Format("Qz: {0:0.0000}", z);
-            mTextQw.text = string.Format("Qw: {0:0.0000}", w);
-        }
-        catch (System.Exception ex)
-        {
-            Debug.Log("Rotate exception!!!");
-            Debug.Log(ex.ToString());
+            case 0:
+            default:
+                transform.rotation = new Quaternion(qx, qy, qz, qw);
+                PrintRotateLog(type, "RotateQuaternion", qx, qy, qz, qw);
+                break;
+            case 1:
+                transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(qx, qy, qz, qw), mRotateInterpolant);
+                PrintRotateLog(type, "SmoothRotateQuaternion", qx, qy, qz, qw);
+                break;
+            case 2:
+                transform.eulerAngles = new Vector3(ex, ey, ez);
+                PrintRotateLog(type, "RotateEular", ex, ey, ez);
+                break;
+            case 3:
+                transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(ex, ey, ez), mRotateInterpolant);
+                PrintRotateLog(type, "SmoothRotateEular", ex, ey, ez);
+                break;
         }
     }
 
-    private void SmoothRotate(float x, float y, float z, float w)
+    private void PrintRotateLog(int type, string prefix, float x, float y, float z, float w = 0)
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(x, y, z, w), mRotateInterpolant);
-
         try
         {
-            mTextQx.text = string.Format("Qx: {0:0.0000}", x);
-            mTextQy.text = string.Format("Qy: {0:0.0000}", y);
-            mTextQz.text = string.Format("Qz: {0:0.0000}", z);
-            mTextQw.text = string.Format("Qw: {0:0.0000}", w);
+            switch (type)
+            {
+                case 0:
+                case 1:
+                default:
+                    mTextQx.text = string.Format("Qx: {0:0.0000}", x);
+                    mTextQy.text = string.Format("Qy: {0:0.0000}", y);
+                    mTextQz.text = string.Format("Qz: {0:0.0000}", z);
+                    mTextQw.text = string.Format("Qw: {0:0.0000}", w);
+                    break;
+                case 2:
+                case 3:
+                    mTextEx.text = string.Format("Ex: {0:0.0000}", x);
+                    mTextEy.text = string.Format("Ey: {0:0.0000}", y);
+                    mTextEz.text = string.Format("Ez: {0:0.0000}", z);
+                    break;
+            }
         }
         catch (System.Exception ex)
         {
-            Debug.Log("SmoothRotate exception!!!");
+            Debug.Log(prefix + " exception!!!");
             Debug.Log(ex.ToString());
         }
     }
 
     // Export to set rotation
-    public void setRotation(string xyzw)
+    public void setRotation(string QxyzwExyz)
     {
-        string[] values = xyzw.Split(',');
-        if (values.Length != 4)
+        string[] values = QxyzwExyz.Split(',');
+        if (values.Length != 7)
         {
+            Debug.Log("QxyzwExyz size is less than 7");
             return;
         }
         else
@@ -136,6 +144,9 @@ public class PlayerController : MonoBehaviour
             mQy = float.Parse(values[1]);
             mQz = float.Parse(values[2]);
             mQw = float.Parse(values[3]);
+            mEx = float.Parse(values[4]);
+            mEy = float.Parse(values[5]);
+            mEz = float.Parse(values[6]);
         }
     }
 
@@ -153,34 +164,28 @@ public class PlayerController : MonoBehaviour
     #endregion
 
     #region Movement
-    private void Move(float x, float y, float z)
+    private void Move(int type, float x, float y, float z)
     {
-        transform.position = new Vector3(x * mMoveScale, y * mMoveScale, z * mMoveScale);
-
-        try
+        string prefix = "";
+        switch (type)
         {
-            // Movement
-            mTextMx.text = string.Format("X: {0:0.0000}", x);
-            mTextMy.text = string.Format("Y: {0:0.0000}", y);
-            mTextMz.text = string.Format("Z: {0:0.0000}", z);
+            case 0:
+            default:
+                transform.position = new Vector3(x * mMoveScale, y * mMoveScale, z * mMoveScale);
+                prefix = "Move";
+                break;
+            case 1:
+                transform.position = Vector3.Lerp(transform.position,
+                    new Vector3(x * mMoveScale, y * mMoveScale, z * mMoveScale), mMoveInterpolant);
+                prefix = "SmoothMove";
+                break;
+        }
 
-            // Current player position
-            mTextPx.text = string.Format("Px: {0:0.0000}", transform.position.x);
-            mTextPy.text = string.Format("Py: {0:0.0000}", transform.position.y);
-            mTextPz.text = string.Format("Pz: {0:0.0000}", transform.position.z);
-        }
-        catch (System.Exception ex)
-        {
-            Debug.Log("Move exception!!!");
-            Debug.Log(ex.ToString());
-        }
+        PrintMoveLog(prefix, x, y, z);
     }
 
-    private void SmoothMove(float x, float y, float z)
+    private void PrintMoveLog(string prefix, float x, float y, float z)
     {
-        transform.position = Vector3.Lerp(transform.position,
-            new Vector3(x * mMoveScale, y * mMoveScale, z * mMoveScale), mMoveInterpolant);
-
         try
         {
             // Movement
@@ -195,7 +200,7 @@ public class PlayerController : MonoBehaviour
         }
         catch (System.Exception ex)
         {
-            Debug.Log("SmoothMove exception!!!");
+            Debug.Log(prefix + "exception!!!");
             Debug.Log(ex.ToString());
         }
     }
